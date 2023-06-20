a26253f79c99ef689e4fd1fb641f1ad001d09f1d
He Lin <helinchooi@gmail.com>
2023-06-21 00:23:27 +0800

    Can run app

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,12 +1,13 @@
 import * as firebase from "firebase";
 
 const config = {
-    apiKey: `${process.env.REACT_APP_FIREBASE_API_KEY}`,
-    authDomain: "expense-manager-react.firebaseapp.com",
-    databaseURL: "https://expense-manager-react.firebaseio.com",
-    projectId: "expense-manager-react",
-    storageBucket: "expense-manager-react.appspot.com",
-    messagingSenderId: "847044394910"
+  apiKey: `${process.env.REACT_APP_FIREBASE_API_KEY}`,
+  authDomain: "expense-manager-62883.firebaseapp.com",
+  databaseURL: "https://expense-manager-62883-default-rtdb.firebaseio.com",
+  projectId: "expense-manager-62883",
+  storageBucket: "expense-manager-62883.appspot.com",
+  messagingSenderId: "943185529623",
+  appId: "1:943185529623:web:8525345066625b55938552"
 };
 
 if (!firebase.apps.length) {
